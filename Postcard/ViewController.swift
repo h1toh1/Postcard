57e3603be11159bb1117236a25ac4a9283765db0
h1toh1 <h1toh1@outlook.com>
2015-05-26 17:39:32 -0500

    new label

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -12,12 +12,13 @@ class ViewController: UIViewController {
 
     
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var newmessageLable: UILabel!
     
     @IBOutlet weak var enterNameTestField: UITextField!
-    
     @IBOutlet weak var enterMessageTextField: UITextField!
     
     @IBOutlet weak var sendButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,8 +37,15 @@ class ViewController: UIViewController {
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
         
+        newmessageLable.hidden = false
+        newmessageLable.text = enterNameTestField.text
+        newmessageLable.textColor = UIColor.blueColor()
+        
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
+        
+        enterNameTestField.text = ""
+        enterNameTestField.resignFirstResponder()
         
         sendButton.setTitle("Mail Sent", forState: UIControlState.Normal)
     }
